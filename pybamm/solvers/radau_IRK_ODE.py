66c8cc1bce05cc56b385af974186a8d0c99f613c
Jason Siegel <siegeljb@umich.edu>
2020-06-11 01:16:04 -0400

    integration working but does not pass the assertion, may need to adjust time steps, since fixed integration

--- pybamm/solvers/radau_IRK_ODE.py
+++ pybamm/solvers/radau_IRK_ODE.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
-from casadi import *
-import numpy as N
-import matplotlib.pyplot as plt
+# from casadi import *
+# import numpy as N
+# import matplotlib.pyplot as plt
+
+
+import casadi
+import pybamm
+import numpy as np
+from scipy.interpolate import interp1d
+from scipy.optimize import brentq
+
 
 """
 Demonstration on how to construct a fixed-step implicit Runge-Kutta integrator
@@ -16,8 +24,8 @@ n_x = 1
 n_p = 0
 
 # Declare variables
-x = SX.sym("x", n_x)  # state
-p = SX.sym("u", n_p)  # control
+x = casadi.SX.sym("x", n_x)  # state
+p = casadi.SX.sym("u", n_p)  # control
 
 # ODE right hand side function
 # ode = vertcat((1 - x[1]*x[1])*x[0] - x[1] + p, \
@@ -26,7 +34,7 @@ p = SX.sym("u", n_p)  # control
 
 ode = 0.1 * x[0]
 dae = {"x": x, "p": p, "ode": ode}
-f = Function("f", [x, p], [ode])
+f = casadi.Function("f", [x, p], [ode])
 
 # Number of finite elements
 n = 100
@@ -38,16 +46,16 @@ h = tf / n
 d = 4
 
 # Choose collocation points
-tau_root = [0] + collocation_points(d, "legendre")
+tau_root = [0] + casadi.collocation_points(d, "legendre")
 
 # Coefficients of the collocation equation
-C = N.zeros((d + 1, d + 1))
+C = np.zeros((d + 1, d + 1))
 
 # Coefficients of the continuity equation
-D = N.zeros(d + 1)
+D = np.zeros(d + 1)
 
 # Dimensionless time inside one control interval
-tau = SX.sym("tau")
+tau = casadi.SX.sym("tau")
 
 # For all collocation points
 for j in range(d + 1):
@@ -58,21 +66,21 @@ for j in range(d + 1):
             L *= (tau - tau_root[r]) / (tau_root[j] - tau_root[r])
 
     # Evaluate the polynomial at the final time to get the coefficients of the continuity equation
-    lfcn = Function("lfcn", [tau], [L])
+    lfcn = casadi.Function("lfcn", [tau], [L])
     D[j] = lfcn(1.0)
 
     # Evaluate the time derivative of the polynomial at all collocation points to get the coefficients of the continuity equation
-    tfcn = Function("tfcn", [tau], [tangent(L, tau)])
+    tfcn = casadi.Function("tfcn", [tau], [casadi.tangent(L, tau)])
     for r in range(d + 1):
         C[j, r] = tfcn(tau_root[r])
 
 # Total number of variables for one finite element
-X0 = MX.sym("X0", n_x)
-P = MX.sym("P", n_p)
-V = MX.sym("V", d * n_x)
+X0 = casadi.MX.sym("X0", n_x)
+P = casadi.MX.sym("P", n_p)
+V = casadi.MX.sym("V", d * n_x)
 
 # Get the state at each collocation point
-X = [X0] + vertsplit(V, [r * n_x for r in range(d + 1)])
+X = [X0] + casadi.vertsplit(V, [r * n_x for r in range(d + 1)])
 
 # Get the collocation quations (that define V)
 V_eq = []
@@ -87,17 +95,17 @@ for j in range(1, d + 1):
     V_eq.append(h * f_j - xp_j)
 
 # Concatenate constraints
-V_eq = vertcat(*V_eq)
+V_eq = casadi.vertcat(*V_eq)
 
 # Root-finding function, implicitly defines V as a function of X0 and P
-vfcn = Function("vfcn", [V, X0, P], [V_eq])
+vfcn = casadi.Function("vfcn", [V, X0, P], [V_eq])
 
 # Convert to SX to decrease overhead
 vfcn_sx = vfcn.expand()
 
 # Create a implicit function instance to solve the system of equations
-ifcn = rootfinder("ifcn", "newton", vfcn_sx)
-V = ifcn(MX(), X0, P)
+ifcn = casadi.rootfinder("ifcn", "fast_newton", vfcn_sx)
+V = ifcn(casadi.MX(), X0, P)
 X = [X0 if r == 0 else V[(r - 1) * n_x : r * n_x] for r in range(d + 1)]
 
 # Get an expression for the state at the end of the finie element
@@ -106,21 +114,21 @@ for r in range(d + 1):
     XF += D[r] * X[r]
 
 # Get the discrete time dynamics
-F = Function("F", [X0, P], [XF])
+F = casadi.Function("F", [X0, P], [XF])
 
 # Do this iteratively for all finite elements
-Xs = casadi.MX(n_x, n)
+Xs = casadi.MX(n_x, n+1)
 Xs[:, 0] = X0
 X = X0
-for i in range(0,n-1):
+for i in range(0,n):
     X = F(X, P)
     XZ = F(Xs[:, i], P)
     Xs[:, i + 1] = XZ[0]
-X = F(X, P)
+# X = F(X, P)
 
 # Fixed-step integrator
-irk_integrator = Function(
-    "irk_integrator", {"x0": X0, "p": P, "xf": Xs}, integrator_in(), integrator_out()
+irk_integrator = casadi.Function(
+    "irk_integrator", {"x0": X0, "p": P, "xf": Xs}, casadi.integrator_in(), casadi.integrator_out()
 )
 sol_irk =irk_integrator(x0=1, p=[])
 print(sol_irk['xf'])
--- pybamm/solvers/radau_collocation_solver.py
+++ pybamm/solvers/radau_collocation_solver.py
@@ -359,9 +359,9 @@ class ImplicitRandauSolver(pybamm.BaseSolver):
         n_z = algebraic(t_eval[0], y0, inputs).shape[0]
 
         # Declare variables
-        x = casadi.MX.sym("x", n_x)  # state
-        p = casadi.MX.sym("u", n_p)  # control
-        z = casadi.MX.sym("z", n_z)  # algeb state
+        x = casadi.SX.sym("x", n_x)  # state
+        p = casadi.SX.sym("u", n_p)  # control
+        z = casadi.SX.sym("z", n_z)  # algeb state
 # declare a time variable here... todo.
         yfull=casadi.vertcat(x, z)
 
@@ -380,10 +380,16 @@ class ImplicitRandauSolver(pybamm.BaseSolver):
         # 0 = fA(x, z, p, t) at all times by means of the implicit function theorem,
         # implying in particular that ∂fA/∂z must be invertible.
 
+        # Number of finite elements
+        # n = 100
+
+        # Size of the finite elements
+        # h = tf / n
+
         # Degree of interpolating polynomial
         d = 4
 
-        # Choose collocation points "radau"
+        # Choose collocation points
         tau_root = [0] + casadi.collocation_points(d, "legendre")
 
         # Coefficients of the collocation equation
@@ -397,23 +403,20 @@ class ImplicitRandauSolver(pybamm.BaseSolver):
 
         # For all collocation points
         for j in range(d + 1):
-            # Construct Lagrange polynomials to get the polynomial basis at the
-            # collocation point
+            # Construct Lagrange polynomials to get the polynomial basis at the collocation point
             L = 1
             for r in range(d + 1):
                 if r != j:
                     L *= (tau - tau_root[r]) / (tau_root[j] - tau_root[r])
 
-        # Evaluate the polynomial at the final time to get the coefficients of the
-        #  continuity equation
-        lfcn = casadi.Function("lfcn", [tau], [L])
-        D[j] = lfcn(1.0)
+            # Evaluate the polynomial at the final time to get the coefficients of the continuity equation
+            lfcn = casadi.Function("lfcn", [tau], [L])
+            D[j] = lfcn(1.0)
 
-        # Evaluate the time derivative of the polynomial at all collocation points to
-        #  get the coefficients of the continuity equation
-        tfcn = casadi.Function("tfcn", [tau], [casadi.tangent(L, tau)])
-        for r in range(d + 1):
-            C[j, r] = tfcn(tau_root[r])
+            # Evaluate the time derivative of the polynomial at all collocation points to get the coefficients of the continuity equation
+            tfcn = casadi.Function("tfcn", [tau], [casadi.tangent(L, tau)])
+            for r in range(d + 1):
+                C[j, r] = tfcn(tau_root[r])
 
         # Total number of variables for one finite element
         X0 = casadi.MX.sym("X0", n_x)
@@ -431,27 +434,87 @@ class ImplicitRandauSolver(pybamm.BaseSolver):
 
             
 
+
+
+            # # End time
+            # tf = 1.0
+
+            # # Dimensions
+            # n_x = 1
+            # n_p = 0
+
+            # # Declare variables
+            # x = casadi.SX.sym("x", n_x)  # state
+            # p = casadi.SX.sym("u", n_p)  # control
+
+            # ODE right hand side function
+            # ode = vertcat((1 - x[1]*x[1])*x[0] - x[1] + p, \
+            #   x[0], \
+            #   x[0]*x[0] + x[1]*x[1] + p*p)
+
+            # ode = 0.1 * x[0]
+            # dae = {"x": x, "p": p, "ode": ode}
+            # f = casadi.Function("f", [x, p], [ode])
+
+            f = casadi.Function("f", [x, p], [ode])
+         
+
+            # # Number of finite elements
+            # n = 100
+
+            # # Size of the finite elements
+            # h = tf / n
+
+            # # Degree of interpolating polynomial
+            # d = 4
+
+            # # Choose collocation points
+            # tau_root = [0] + casadi.collocation_points(d, "legendre")
+
+            # # Coefficients of the collocation equation
+            # C = np.zeros((d + 1, d + 1))
+
+            # # Coefficients of the continuity equation
+            # D = np.zeros(d + 1)
+
+            # # Dimensionless time inside one control interval
+            # tau = casadi.SX.sym("tau")
+
+            # # For all collocation points
+            # for j in range(d + 1):
+            #     # Construct Lagrange polynomials to get the polynomial basis at the collocation point
+            #     L = 1
+            #     for r in range(d + 1):
+            #         if r != j:
+            #             L *= (tau - tau_root[r]) / (tau_root[j] - tau_root[r])
+
+            #     # Evaluate the polynomial at the final time to get the coefficients of the continuity equation
+            #     lfcn = casadi.Function("lfcn", [tau], [L])
+            #     D[j] = lfcn(1.0)
+
+            #     # Evaluate the time derivative of the polynomial at all collocation points to get the coefficients of the continuity equation
+            #     tfcn = casadi.Function("tfcn", [tau], [casadi.tangent(L, tau)])
+            #     for r in range(d + 1):
+            #         C[j, r] = tfcn(tau_root[r])
+
+            # Total number of variables for one finite element
+            X0 = casadi.MX.sym("X0", n_x)
+            P = casadi.MX.sym("P", n_p)
+            V = casadi.MX.sym("V", d * n_x)
+
             # Get the state at each collocation point
-            X = [X0] + casadi.vertsplit(Vx, [r * n_x for r in range(d + 1)])
+            X = [X0] + casadi.vertsplit(V, [r * n_x for r in range(d + 1)])
 
-            ode = rhs(
-                t_eval[0], x, p
-            )  # vertcat(0.7*x[1]+sin(2.5*z[0]),1.4*x[0]+cos(2.5*z[0]))
-
-            #fx = casadi.Function("fx", [x, p], [ode])
-            fx = casadi.Function("fx", [x, p], [0.1*x[0]])
-
-            # Get the collocation equations (that define V)
+            # Get the collocation quations (that define V)
             V_eq = []
             for j in range(1, d + 1):
                 # Expression for the state derivative at the collocation point
                 xp_j = 0
-                #  zp_j = 0
                 for r in range(d + 1):
                     xp_j += C[r, j] * X[r]
-                #  zp_j += C[r,j]*Z[r]
+
                 # Append collocation equations
-                f_j = fx(X[j], P)
+                f_j = f(X[j], P)
                 V_eq.append(h * f_j - xp_j)
 
             # Concatenate constraints
@@ -459,60 +522,57 @@ class ImplicitRandauSolver(pybamm.BaseSolver):
 
             # Root-finding function, implicitly defines V as a function of X0 and P
             vfcn = casadi.Function("vfcn", [V, X0, P], [V_eq])
-            # vfcn = Function('vfcn', [V, X0,Z0_guess,P], [V_eq])
 
             # Convert to SX to decrease overhead
             vfcn_sx = vfcn.expand()
 
+            # Create a implicit function instance to solve the system of equations
+            # ifcn = casadi.rootfinder("ifcn", "fast_newton", vfcn_sx)
             opts = {}
             opts["max_iter"] = 1000
             # opts['reltol']=1e-6
             opts["abstol"] = 1e-7
             # Create a implicit function instance to solve the system of equations
             ifcn = casadi.rootfinder("ifcn", "fast_newton", vfcn_sx, opts)
-            init_guess = casadi.MX.zeros(d * n_x, 1)
-            V = ifcn(init_guess, X0, P)
-            # X = [X0 if r==0 else V[(r-1)*nx:r*nx] for r in range(d+1)]
 
-            X = [X0 if r == 0 else V[(r - 1) * (n_x) : r * n_x] for r in range(d + 1)]
+            V = ifcn(casadi.MX(), X0, P)
+            X = [X0 if r == 0 else V[(r - 1) * n_x : r * n_x] for r in range(d + 1)]
 
             # Get an expression for the state at the end of the finie element
             XF = 0
-
             for r in range(d + 1):
                 XF += D[r] * X[r]
 
             # Get the discrete time dynamics
-            # F = Function('F', [X0,P],[XF,ZF,Z0])
-            F = casadi.Function("F", [X0, P], [XF])  #
+            F = casadi.Function("F", [X0, P], [XF])
 
             # Do this iteratively for all finite elements
-            # # change back for symbolic eval of integrator
             Xs = casadi.MX(n_x, n)
             Zs = casadi.MX(n_z, n)
             Xs[:, 0] = X0
-
-            for i in range(n - 1):
+            X = X0
+            # ts== casadi.MX(1 n)
+            # ts[0]=0
+            
+            for i in range(0,n-1):
+                X = F(X, P)
                 XZ = F(Xs[:, i], P)
                 Xs[:, i + 1] = XZ[0]
+                ts[i+1]
+            X = F(X, P)
 
-            # ts = np.linspace(0, tf, n)
-            # #integrator = integrator('integrator', 'cvodes', dae, {'grid':ts, 'output_t0':True})
-            # mirk_integrator = Function('mirk_integrator', {'x0':X0, 'z0':Z0, 'p':P, 'xf':X},
-            #                           integrator_in(), integrator_out())
-
+            # Fixed-step integrator
+            irk_integrator = casadi.Function(
+                "irk_integrator", {"x0": X0,'z0':Z0_guess,"p": P, "xf": Xs, "zf": Zs}, casadi.integrator_in(), casadi.integrator_out()
+            )
+            # sol_irk =irk_integrator(x0=1, p=[])
+            # print(sol_irk['xf'])
             # irk_integrator = casadi.Function(
-            #     "irk_integrator_ODE",
-            #     {"x0": X0, "p": P, "xf": Xs,},
+            #     "irk_integrator",
+            #     {"x0": X0, "z0": Z0_guess, "p": P, "xf": Xs, "zf": Zs},
             #     casadi.integrator_in(),
             #     casadi.integrator_out(),
             # )
-            irk_integrator = casadi.Function(
-                "irk_integrator",
-                {"x0": X0, "z0": Z0_guess, "p": P, "xf": Xs, "zf": Zs},
-                casadi.integrator_in(),
-                casadi.integrator_out(),
-            )
 
         else:
 
@@ -589,7 +649,7 @@ class ImplicitRandauSolver(pybamm.BaseSolver):
             # Get the discrete time dynamics
             # F = Function('F', [X0,P],[XF,ZF,Z0])
             F = casadi.Function(
-                "F", [X0, Z0_guess, P], [XF, ZF, Z0, Z[0], X[0], Z[1], X[1]]
+                "F", [X0, Z0_guess, P], [XF, ZF, Z0] #
             )  #
 
             # Do this iteratively for all finite elements
@@ -639,6 +699,7 @@ class ImplicitRandauSolver(pybamm.BaseSolver):
 
             y_values = np.concatenate([sol["xf"].full(), sol["zf"].full()])
             return pybamm.Solution(t_eval, y_values)
+
         except RuntimeError as e:
             # If it doesn't work raise error
             raise pybamm.SolverError(e.args[0])
